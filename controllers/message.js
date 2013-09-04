2700eaba0bed2f139dbbee2369c8283003da53f6
neoadventist <git@spencersims.com>
2013-09-04 16:49:16 -0700

    Message string changed into array of objects

--- controllers/message.js
+++ controllers/message.js
@@ -1,4 +1,15 @@
 app.controller('message', function ($scope, $timeout, $filter) {
-		$scope.header="Message!!";
+	$scope.header="Message!!";
+	$scope.m = [];
+	$scope.process = function(){
+		$scope.m = [];
+		m = $scope.message.split('');
+		for(i=0;i<$scope.message.length;i++){
+			$scope.m.push({original:m[i],translated:""});
+		}
 
+	}
+	
+	console.log($scope.m);
+	
 });
