1040e4d74b42da287752eaa4b69a130a39b3e01d
neoadventist <git@spencersims.com>
2013-09-06 03:37:01 -0700

    fixed bug which did not update more than two fields of the same letter

--- directives.js
+++ directives.js
@@ -11,16 +11,20 @@ app.directive('transLation', function() {
 		restrict: 'A',
 		link: function(scope, element, attrs) {
 			//tests to see if every letter is unique
-			var oneToOne = function(test,m){
+			var oneToOne = function(orig,test,m){
 				var match=false;
 				var count=0;
+				var occurance =0;
 				for(n=0;n<m.length;n++){
 					console.log(m[n].translated);
 					if(test==m[n].translated){
 						count++;
 					}
+					if(orig==m[n].original){
+						occurance++;
+					}
 				}
-				if(count>=2){ 
+				if(count>=occurance && count>1){ 
 					//count is two beacuse the copied letter is automatically entered into the object via angular binding. 
 					match=true;
 				}
@@ -29,13 +33,13 @@ app.directive('transLation', function() {
 			scope.translate = function(oldletter,newletter){
 			console.log(scope.message);
 				for(i=0;i<scope.message.length;i++){
-					if(!oneToOne(newletter,scope.message)){
+					if(!oneToOne(oldletter,newletter,scope.message)){
 						if(scope.message[i].original==oldletter){
 							scope.message[i].translated=newletter;
 						}
 					}else{
 						if(scope.message[i].original==oldletter){
-							scope.message[i].translated=''; //find the angular binded value and set it equal to nothing.  
+							scope.message[i].translated=undefined; //find the angular binded value and set it equal to nothing.  
 						}
 					}
 				}
