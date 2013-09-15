6c82dea5686e368d937503faf259b4e9afe0d3f4
neoadventist <git@spencersims.com>
2013-09-14 23:56:07 -0700

    changed from 'message' to 'state'

---
+++ controllers/state.js
@@ -0,0 +1,26 @@
+app.controller('state', function ($scope, $timeout, $filter) {
+	$scope.header="Message!!";
+	
+	var asciiToHex = function(ascii){
+		return ascii.charCodeAt(0).toString(16);
+	}
+	
+	var hexToascii = function(hex){
+		return String.fromCharCode(parseInt(n, 16));
+	}
+	
+	
+	$scope.m = [];
+	$scope.process = function(){
+		$scope.m = [];
+		m = $scope.message.split('');
+		for(i=0;i<$scope.message.length;i++){
+			$scope.m.push({ascii:m[i],hex:asciiToHex(m[i])});
+		}
+
+	}
+	
+	console.log($scope.m);
+	
+
+});
